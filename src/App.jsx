689047e5be31efe8003e0045afd22d32e9c14460
takabu152 <yamashitat@libre-code.com>
2020-08-21 16:26:20 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <Router>
-      <Route path="/login">
+      <Route path="/">
         <Login count={count} setCount = {setCount} />
 			</Route>
       <Route path="/mainmenu">
